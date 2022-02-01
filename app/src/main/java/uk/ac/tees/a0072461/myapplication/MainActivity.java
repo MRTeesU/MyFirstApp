066f375f600d208676f67359df5523efb118c541
MRTeesU <A0072461@live.tees.ac.uk>
2022-02-01 13:21:30 +0000

    unaltered empty project

---
+++ app/src/main/java/uk/ac/tees/a0072461/myapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package uk.ac.tees.a0072461.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
